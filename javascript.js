aeb912a5598c0a63ce79268b2bfc3a4a1da0ca90
JordyNtambwe <jordyntambwe@gmail.com>
2026-02-27 14:53:30 -0700

    added an EventListener that results in a trail like effect the moment the cursor touch a square

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 const container = document.querySelector('#container')
-// This piece of code builds the 16x16 grid in seconds instead of creating a div that needs to be manually typed out 256 times
+// This piece of code builds the 16x16 grid instead of creating a div that needs to be manually typed out 256 times
 const totalSquares = 256;
 for (let i = 0; i < totalSquares; i++) {
   const square = document.createElement('div');
   square.classList.add('grid');
+// This EventListener triggers the moment the cursor touches a square and changing the color to black, resulting in a trail like effect
+  square.addEventListener('mouseenter', () => {
+    square.style.backgroundColor = 'black';
+  });
   container.appendChild(square);
 };
